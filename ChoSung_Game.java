b0608f9477627efc321050d3388a9afaeb0adf46
Lee-SungMin <saevers26@gmail.com>
2019-11-25 15:47:28 +0900

    2번째 커밋-게임 시작화면 제작
    게임 이름과 게임 시작 버튼을 추가

--- ChoSung_Game.java
+++ ChoSung_Game.java
@@ -4,10 +4,13 @@ import java.awt.EventQueue;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
+import javax.swing.JLabel;
+import java.awt.Font;
+import javax.swing.JButton;
 
 public class ChoSung_Game extends JFrame {
 
-	private JPanel StartPage;
+	private JPanel MainPage;
 
 	/**
 	 * Launch the application.
@@ -31,10 +34,23 @@ public class ChoSung_Game extends JFrame {
 	public ChoSung_Game() {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setBounds(650, 300, 700, 500);
-		StartPage = new JPanel();
-		StartPage.setBorder(new EmptyBorder(5, 5, 5, 5));
-		setContentPane(StartPage);
+		MainPage = new JPanel();
+		MainPage.setBorder(new EmptyBorder(5, 5, 5, 5));
+		setContentPane(MainPage);
+		MainPage.setLayout(null);
+		
+		JPanel StartPage = new JPanel();
+		StartPage.setBounds(0, 0, 684, 461);
+		MainPage.add(StartPage);
 		StartPage.setLayout(null);
+		
+		JLabel StartLabel = new JLabel("초성퀴즈");
+		StartLabel.setFont(new Font("한컴산뜻돋움", Font.BOLD, 45));
+		StartLabel.setBounds(247, 127, 173, 78);
+		StartPage.add(StartLabel);
+		
+		JButton StartBtn = new JButton("시작하기");
+		StartBtn.setBounds(286, 346, 97, 23);
+		StartPage.add(StartBtn);
 	}
-
 }
