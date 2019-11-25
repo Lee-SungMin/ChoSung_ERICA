3ee94fbc1b73f14ecb67aca782d66b001b1b1ced
Lee-SungMin <saevers26@gmail.com>
2019-11-25 22:05:48 +0900

    3번째 커밋-게임 시작 버튼 활성화
    시작 버튼을 클릭하면 게임 화면으로 넘어가도록 수정

--- ChoSung_Game.java
+++ ChoSung_Game.java
@@ -6,7 +6,11 @@ import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
 import javax.swing.JLabel;
 import java.awt.Font;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
 import javax.swing.JButton;
+import javax.swing.JTextField;
 
 public class ChoSung_Game extends JFrame {
 
@@ -45,12 +49,41 @@ public class ChoSung_Game extends JFrame {
 		StartPage.setLayout(null);
 		
 		JLabel StartLabel = new JLabel("초성퀴즈");
-		StartLabel.setFont(new Font("한컴산뜻돋움", Font.BOLD, 45));
 		StartLabel.setBounds(247, 127, 173, 78);
+		StartLabel.setFont(new Font("한컴산뜻돋움", Font.BOLD, 45));
 		StartPage.add(StartLabel);
 		
 		JButton StartBtn = new JButton("시작하기");
 		StartBtn.setBounds(286, 346, 97, 23);
 		StartPage.add(StartBtn);
+		
+		JPanel GamePage = new JPanel();
+		GamePage.setBounds(0, 0, 684, 461);
+		MainPage.add(GamePage);
+		GamePage.setLayout(null);
+		
+		
+		StartBtn.addActionListener(new ActionListener() {
+
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				StartPage.setVisible(false);
+				GamePage.setVisible(true);
+			}
+			
+		});
+		
+		JPanel Game1 = new JPanel();
+		Game1.setBounds(0, 0, 684, 461);
+		GamePage.add(Game1);
+		Game1.setLayout(null);
+		
+		JLabel StepGame1 = new JLabel("문제 1");
+		StepGame1.setFont(new Font("한컴산뜻돋움", Font.PLAIN, 30));
+		StepGame1.setBounds(38, 33, 90, 39);
+		Game1.add(StepGame1);
+		
+		
 	}
+	
 }
