75385bfd24e58d52a32d0aadc5ca240593850ac8
Lee-SungMin <saevers26@gmail.com>
2019-11-25 22:39:14 +0900

    4번째 커밋-정답시, 다음 단계로 이동 활성화
    문제 1을 맞추면 문제 2로 넘어가고, 맞추치 못하면 계속 도전할 수 있게 페이지에 머물도록 함

--- ChoSung_Game.java
+++ ChoSung_Game.java
@@ -15,6 +15,7 @@ import javax.swing.JTextField;
 public class ChoSung_Game extends JFrame {
 
 	private JPanel MainPage;
+	private JTextField Game1AnsBlank;
 
 	/**
 	 * Launch the application.
@@ -48,6 +49,11 @@ public class ChoSung_Game extends JFrame {
 		MainPage.add(StartPage);
 		StartPage.setLayout(null);
 		
+		JPanel GamePage = new JPanel();
+		GamePage.setBounds(0, 0, 684, 461);
+		MainPage.add(GamePage);
+		GamePage.setLayout(null);
+		
 		JLabel StartLabel = new JLabel("초성퀴즈");
 		StartLabel.setBounds(247, 127, 173, 78);
 		StartLabel.setFont(new Font("한컴산뜻돋움", Font.BOLD, 45));
@@ -57,13 +63,9 @@ public class ChoSung_Game extends JFrame {
 		StartBtn.setBounds(286, 346, 97, 23);
 		StartPage.add(StartBtn);
 		
-		JPanel GamePage = new JPanel();
-		GamePage.setBounds(0, 0, 684, 461);
-		MainPage.add(GamePage);
-		GamePage.setLayout(null);
+		GamePage.setVisible(false);
 		
-		
-		StartBtn.addActionListener(new ActionListener() {
+		StartBtn.addActionListener(new ActionListener() { // 게임 시작 버튼 활성화
 
 			@Override
 			public void actionPerformed(ActionEvent e) {
@@ -73,17 +75,57 @@ public class ChoSung_Game extends JFrame {
 			
 		});
 		
+		
 		JPanel Game1 = new JPanel();
 		Game1.setBounds(0, 0, 684, 461);
 		GamePage.add(Game1);
 		Game1.setLayout(null);
 		
 		JLabel StepGame1 = new JLabel("문제 1");
-		StepGame1.setFont(new Font("한컴산뜻돋움", Font.PLAIN, 30));
 		StepGame1.setBounds(38, 33, 90, 39);
+		StepGame1.setFont(new Font("한컴산뜻돋움", Font.PLAIN, 30));
 		Game1.add(StepGame1);
+		
+		JLabel Game1Quiz = new JLabel("\u3131\u3134");
+		Game1Quiz.setFont(new Font("한컴산뜻돋움", Font.BOLD, 40));
+		Game1Quiz.setBounds(185, 159, 81, 87);
+		Game1.add(Game1Quiz);
+		
+		Game1AnsBlank = new JTextField();
+		Game1AnsBlank.setText("이곳에 정답을 입력하세요");
+		Game1AnsBlank.setBounds(153, 352, 147, 39);
+		Game1.add(Game1AnsBlank);
+		Game1AnsBlank.setColumns(10);
+		
+		JButton Game1AnsBtn = new JButton("제출하기");
+		Game1AnsBtn.setBounds(340, 352, 121, 39);
+		Game1.add(Game1AnsBtn);
+		
+		JPanel Game2 = new JPanel();
+		Game2.setBounds(0, 0, 684, 461);
+		GamePage.add(Game2);
+		Game2.setLayout(null);
+		
+		JLabel StepGame2 = new JLabel("문제 2");
+		StepGame2.setBounds(38, 33, 90, 39);
+		StepGame2.setFont(new Font("한컴산뜻돋움", Font.PLAIN, 30));
+		Game2.add(StepGame2);
+		
+		Game1AnsBtn.addActionListener(new ActionListener() { // 문제 1 정답 확인
+
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				String Answer = "가나";
+				if(Answer.equals(Game1AnsBlank.getText())) {
+					Game1.setVisible(false);
+					Game2.setVisible(true);
+				}
+				
+				
+			}
+			
+		});
 		
 		
 	}
-	
 }
