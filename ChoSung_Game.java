ea5104dd101f86a34cda7a9c18f689b3d8f9c0ef
Lee-SungMin <saevers26@gmail.com>
2019-11-27 11:34:51 +0900

    9번쨰 커밋-게임 설명 페이지 활성화
    게임 설명 및 룰을 적어 사용자의 이해에 도움

--- ChoSung_Game.java
+++ ChoSung_Game.java
@@ -11,6 +11,9 @@ import java.awt.event.ActionListener;
 
 import javax.swing.JButton;
 import javax.swing.JTextField;
+import javax.swing.SwingConstants;
+import java.awt.Color;
+import java.awt.SystemColor;
 
 public class ChoSung_Game extends JFrame {
 
@@ -50,58 +53,124 @@ public class ChoSung_Game extends JFrame {
 		MainPage.add(StartPage);
 		StartPage.setLayout(null);
 		
-		JLabel StartLabel = new JLabel("초성퀴즈");
-		StartLabel.setBounds(247, 127, 173, 78);
+		JPanel SelectTemaPage = new JPanel();
+		SelectTemaPage.setBounds(0, 0, 684, 461);
+		MainPage.add(SelectTemaPage);
+		SelectTemaPage.setLayout(null);
+		
+		SelectTemaPage.setVisible(false);
+		
+		JPanel GameExpPage = new JPanel();
+		GameExpPage.setBounds(0, 0, 684, 461);
+		MainPage.add(GameExpPage);
+		GameExpPage.setLayout(null);
+		
+		GameExpPage.setVisible(false);
+		
+		JLabel StartLabel = new JLabel("초성 퀴즈 게임");
+		StartLabel.setBounds(192, 127, 276, 78);
 		StartLabel.setFont(new Font("한컴산뜻돋움", Font.BOLD, 45));
 		StartPage.add(StartLabel);
 		
-		JButton StartBtn = new JButton("시작하기");
-		StartBtn.setBounds(286, 346, 97, 23);
+		JButton StartBtn = new JButton("게임 시작");
+		StartBtn.setBounds(225, 346, 97, 23);
 		StartPage.add(StartBtn);
 		
-		JPanel GamePage = new JPanel();
-		GamePage.setBounds(0, 0, 684, 461);
-		MainPage.add(GamePage);
-		GamePage.setLayout(null);
-		
-		JPanel SelectTema = new JPanel();
-		SelectTema.setBounds(0, 0, 684, 461);
-		MainPage.add(SelectTema);
-		SelectTema.setLayout(null);
+		JButton GameExpBtn = new JButton("게임 설명");
+		GameExpBtn.setBounds(345, 346, 97, 23);
+		StartPage.add(GameExpBtn);
 		
 		StartBtn.addActionListener(new ActionListener() { // 게임 시작 버튼 활성화
 
 			@Override
 			public void actionPerformed(ActionEvent e) {
 				StartPage.setVisible(false);
-				SelectTema.setVisible(true);
+				SelectTemaPage.setVisible(true);
 			}
 			
 		});
 		
+		GameExpBtn.addActionListener(new ActionListener(){ // 게임 설명 버튼 활성화
+
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				StartPage.setVisible(false);
+				GameExpPage.setVisible(true);
+			}
+			
+		});
+		
+		
+		JLabel GameExpTitle = new JLabel("<게임 설명>");
+		GameExpTitle.setFont(new Font("한컴산뜻돋움", Font.BOLD, 35));
+		GameExpTitle.setBounds(35, 42, 173, 46);
+		GameExpPage.add(GameExpTitle);
+		
+		JLabel GameExp1 = new JLabel("-초성 퀴즈란?");
+		GameExp1.setVerticalAlignment(SwingConstants.TOP);
+		GameExp1.setFont(new Font("한컴산뜻돋움", Font.PLAIN, 16));
+		GameExp1.setBounds(35, 118, 110, 21);
+		GameExpPage.add(GameExp1);
+		
+		JLabel GameExp1_1 = new JLabel("한글을 이용한 놀이 중 하나로, 어떤 단어의 초성만을 알려주고 그 단어를 알아맞히는 놀이이다.");
+		GameExp1_1.setForeground(Color.DARK_GRAY);
+		GameExp1_1.setFont(new Font("한컴산뜻돋움", Font.PLAIN, 14));
+		GameExp1_1.setBounds(35, 149, 557, 21);
+		GameExpPage.add(GameExp1_1);
+		
+		JLabel GameExp2 = new JLabel("-게임 룰 설명");
+		GameExp2.setFont(new Font("한컴산뜻돋움", Font.PLAIN, 16));
+		GameExp2.setBounds(35, 203, 110, 21);
+		GameExpPage.add(GameExp2);
+		
+		JLabel GameExp2_1 = new JLabel("(미정)");
+		GameExp2_1.setFont(new Font("한컴산뜻돋움", Font.PLAIN, 14));
+		GameExp2_1.setForeground(Color.DARK_GRAY);
+		GameExp2_1.setBounds(35, 234, 57, 15);
+		GameExpPage.add(GameExp2_1);
+		
+		JButton StartBackBtn1 = new JButton("시작 화면으로");
+		StartBackBtn1.setBounds(482, 404, 110, 23);
+		GameExpPage.add(StartBackBtn1);
+		
+		StartBackBtn1.addActionListener(new ActionListener() { // 시작 화면으로 돌아가는 버튼 활성화
+
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				GameExpPage.setVisible(false);
+				StartPage.setVisible(true);
+			}
+			
+		});
+		
+		
 		JLabel TemaLabel = new JLabel("<테마 선택>");
 		TemaLabel.setBounds(255, 42, 173, 46);
 		TemaLabel.setFont(new Font("한컴산뜻돋움", Font.BOLD, 35));
-		SelectTema.add(TemaLabel);
+		SelectTemaPage.add(TemaLabel);
 		
 		JButton Tema1_BrandBtn = new JButton("브랜드");
 		Tema1_BrandBtn.setBounds(294, 127, 97, 23);
-		SelectTema.add(Tema1_BrandBtn);
+		SelectTemaPage.add(Tema1_BrandBtn);
 		
+		JPanel GamePage = new JPanel();
+		GamePage.setBounds(0, 0, 684, 461);
+		MainPage.add(GamePage);
+		GamePage.setLayout(null);
+		
+		GamePage.setVisible(false);
 		
 		Tema1_BrandBtn.addActionListener(new ActionListener() { // 테마 선택 버튼 활성화
 
 			@Override
 			public void actionPerformed(ActionEvent e) {
-				SelectTema.setVisible(false);
+				SelectTemaPage.setVisible(false);
 				GamePage.setVisible(true);
 				
 			}
 			
 		});
 		
-		
-		GamePage.setVisible(false);
 		
 		JPanel Game1 = new JPanel();
 		Game1.setBounds(0, 0, 684, 461);
